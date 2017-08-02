e92a5f2f0d72c2a1efd3aefef82784f99e4d2cae
Martin Hatina <mhatina@redhat.com>
2017-08-02 10:49:58 +0200

    fixup! add more short abbrevations of commands

--- dnf/cli/commands/updateinfo.py
+++ dnf/cli/commands/updateinfo.py
@@ -63,7 +63,7 @@ class UpdateInfoCommand(commands.Command):
                        'info-sec'           : 'info',
                        'summary-updateinfo' : 'summary'}
 
-    aliases = ['updateinfo', 'uinfo',] + list(direct_commands.keys())
+    aliases = ['updateinfo', 'uinfo'] + list(direct_commands.keys())
     summary = _('display advisories about packages')
 
     def __init__(self, cli):
